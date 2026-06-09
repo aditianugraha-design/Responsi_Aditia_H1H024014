74ec8b0223a3b80daab221eb2c8bd2402e5e69c8
aditianugraha-design <aditia.nugraha@mhs.unsoed.ac.id>
2026-06-10 01:24:20 +0700

    Add files via upload

---
+++ WEB 1/index.php
@@ -0,0 +1,17 @@
+<?php
+$nama = "Muhammad Nabil Zaedan Agesy";
+$nim  = "H1H024062";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Web 1</title>
+</head>
+<body>
+<h1>WEB SERVER 1</h1>
+<hr>
+<p>Nama Praktikan: <strong><?= $nama ?></strong></p>
+<p>NIM: <strong><?= $nim ?></strong></p>
+<p>Container: <strong>WEB-1</strong></p>
+</body>
+</html>
